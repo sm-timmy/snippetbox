03eac1713a3ba94df56215d682bdb64f50119e5c
caa <caa.timmy@gmail.com>
2023-11-20 15:12:59 +0300

    Chapter 8. Processing forms

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,18 +4,13 @@ import (
 	"artchernov.ru/internal/models"
 	"errors"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
 	"strings"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.Header().Set("Allow", http.MethodGet)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -31,13 +26,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.Header().Set("Allow", http.MethodGet)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -61,30 +51,24 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		title := "O snail"
-		content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-		expires := 7
+	w.Write([]byte("Display the form for creating a new snippet..."))
+	//data := app.newTemplateData(r)
+	//app.render(w, http.StatusOK,
+	//	"form.html",
+	//	data)
+}
 
-		id, err := app.snippets.Insert(title, content, expires)
-		if err != nil {
-			app.serverError(w, err)
-			return
-		}
-		http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
-	} else if r.Method == http.MethodGet {
-		data := app.newTemplateData(r)
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
 
-		app.render(w, http.StatusOK,
-			"form.html",
-			data)
-	} else {
-		w.Header().Set("Allow", http.MethodPost)
-		w.Header().Set("Allow", http.MethodGet)
-		app.clientError(w, http.StatusMethodNotAllowed)
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
 		return
 	}
-
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
 
 func (app *application) neuter(next http.Handler) http.Handler {
@@ -105,7 +89,6 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverError(w, fmt.Errorf("%s", err))
 			}
 		}()
-
 		next.ServeHTTP(w, r)
 	})
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"github.com/julienschmidt/httprouter" // New import
 	"github.com/justinas/alice"
 	"net/http"
 )
 
 func (app *application) routes() http.Handler {
+	// Initialize the router.
+	router := httprouter.New()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	// Create a handler function which wraps our notFound() helper, and then
+	// assign it as the custom handler for 404 Not Found responses. You can also
+	// set a custom handler for 405 Method Not Allowed responses by setting
+	// router.MethodNotAllowed in the same way too.
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 
+	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.clientError(w, http.StatusMethodNotAllowed)
+	})
+
+	// Update the pattern for the route for the static files.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", app.neuter(fileServer)))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	defaultMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	// And then create the routes using the appropriate methods, patterns and
+	// handlers.
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
-	return defaultMiddleware.Then(mux)
+	// Create the middleware chain as normal.
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	// Wrap the router with the middleware and return it as normal.
+	return standard.Then(router)
 }
