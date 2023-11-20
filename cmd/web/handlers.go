c1642499d8d8c56a52fdce229e00225dcbee4d7b
caa <caa.timmy@gmail.com>
2023-11-20 12:03:36 +0300

    Chapter 6.5. Composable middleware chains

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -77,3 +77,16 @@ func (app *application) neuter(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
